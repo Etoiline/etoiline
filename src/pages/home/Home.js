519ba11a4c66da5a5e6c50a25b3fffac44c78eca
Laura Casteran <laurac644@gmail.com>
2022-09-15 19:36:32 +0200

    react gh pages

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,12 @@
 import home from './Home.module.css'
-import Photo from '../../assets/photo.png'
+import Photo from '../../assets/DSC_6610.JPG'
 import React from 'react';
 
 function Home() {
   return (
     <div className={home.main}>
       <div className={home.content}>
-      <img src={Photo} alt='me' />
+      <img className={home.image} src={Photo} alt='me' />
       <div>
       <h1>Laura CASTERAN</h1>
       <h2>Développeur frontend - junior</h2>
--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,9 +1,14 @@
 import portfolio from './Portfolio.module.css'
+import projectList from '../../assets/projects.json'
+import Thumb from '../../component/thumb/Thumb'
 
 function Portfolio() {
+  console.log(projectList)
   return (
     <div className={portfolio.main}>
-PORTFOLIO
+      {projectList.map((value, index) => {
+        return <div key={index} className={portfolio.thumb}><Thumb value={value}/></div>
+      })}
     
     </div>
   )
